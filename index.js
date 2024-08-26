071ce79e0955afa51d29f9d232ca6096ae44472a
itsSunil001 <98368191+itsSunil001@users.noreply.github.com>
2024-08-26 14:12:00 +0530

    add cors url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173","https://goss-app.netlify.app"],
     credentials: true,
     
   },
@@ -45,7 +45,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://goss-app.netlify.app"],
     credentials: true,
   })
 );
